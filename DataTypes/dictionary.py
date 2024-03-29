b3db79b9c7c9d4cc0666ee20c3d36d7dd9d606d0
Jorge Lopez <jlopezvac@gmail.com>
2024-03-29 22:31:20 +0100

    New file

---
+++ DataTypes/dictionary.py
@@ -0,0 +1,8 @@
+my_ditcionary = {"Iron Maiden" : "United Kingdon", "Hammerfall" : "Sweden", "Edguy" : "Germany",
+    "Sonata Arctica" : "Finland", "Testament" : "USA", "Kreator" : "Germany"}
+
+# print the number of items
+print(len(my_ditcionary))
+
+# print the dictionary
+print(my_ditcionary)
