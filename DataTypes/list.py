4989d6e264852af8b1e94ac56787107a321afdea
Jorge Lopez <jlopezvac@gmail.com>
2024-03-29 17:36:59 +0100

    Methods added

--- DataTypes/list.py
+++ DataTypes/list.py
@@ -1,8 +1,27 @@
 # lists
 my_list = ["Fender", "Warwick", "Epiphone", "Yamaha", "Cort", "Jackson"]
 
+# print the number of items
+print(len(my_list))
+
 # print all
 print(my_list)
 
-# get an element
+# print the first element
 print(my_list[0])
+
+# print the last element
+print(my_list[-1])
+
+# print a range
+print(my_list[2:5])
+print(my_list[:3])
+print(my_list[2:])
+print(my_list[1:-1])
+
+# add item
+my_list.append("LTD")
+
+# iterate
+for value in my_list:
+    print(value)
